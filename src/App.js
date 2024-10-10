c0248108394943391893439ff0243e769be22780
nazzar123 <139446554+nazzar123@users.noreply.github.com>
2024-10-10 23:09:27 +0300

    disable vertical

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   useEffect(() => {
     tg.ready()
-    tg.expand()
+    tg.disableVerticalSwipes()
   }, [tg])//удалить tg из зависимостей
 
   const backButton = tg.BackButton
